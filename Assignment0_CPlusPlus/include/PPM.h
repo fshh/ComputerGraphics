19e1a89298219c9dc0922df428c228ff033c5cdc
fshh <ajknollmeyer@gmail.com>
2020-01-17 23:38:13 -0500

    Assignment 0 done

--- Assignment0_CPlusPlus/include/PPM.h
+++ Assignment0_CPlusPlus/include/PPM.h
@@ -1,6 +1,6 @@
 /** @file PPM.h
  *  @brief Class for working with PPM images
- *  
+ *
  *  Class for working with P3 PPM images specifically.
  *
  *  @author your_name_here
@@ -24,7 +24,7 @@ public:
     // in the PPM. Note that no values may be less than
     // 0 in a ppm.
     void darken();
-    // Sets a pixel to a specific R,G,B value 
+    // Sets a pixel to a specific R,G,B value
     void setPixel(int x, int y, int R, int G, int B);
     // Returns the raw pixel data in an array.
     // You may research what 'inline' does.
@@ -33,15 +33,18 @@ public:
     inline int getWidth(){ return m_width; }
     // Returns image height
     inline int getHeight(){ return m_height; }
-// NOTE:    You may add any helper functions you like in the
-//          private section.
-private:    
+// NOTE:    You may add any helper functions you like in the private section.
+private:
     // Store the raw pixel data here
     // Data is R,G,B format
     unsigned char* m_PixelData;
     // Store width and height of image.
     int m_width{0};
     int m_height{0};
+	// Returns true if the given filename ends in .ppm
+	bool isPPMFile(std::string fileName);
+	// Returns the given int value, clamped to the 0-255 range
+	int clampRGB(int val);
 };
 
 
