08e6cd3ca7335e71f54008f7634c3537e5b0f561
fshh <ajknollmeyer@gmail.com>
2020-02-19 18:02:21 -0500

    Set up basics

--- Assignment3_ModelParser/BasicWidget.cpp
+++ Assignment3_ModelParser/BasicWidget.cpp
@@ -2,26 +2,87 @@
 
 //////////////////////////////////////////////////////////////////////
 // Publics
-BasicWidget::BasicWidget(QWidget* parent) : QOpenGLWidget(parent)
+BasicWidget::BasicWidget(QWidget* parent) : QOpenGLWidget(parent), vbo_(QOpenGLBuffer::VertexBuffer), cbo_(QOpenGLBuffer::VertexBuffer), ibo_(QOpenGLBuffer::IndexBuffer)
 {
   setFocusPolicy(Qt::StrongFocus);
 }
 
 BasicWidget::~BasicWidget()
 {
+  vbo_.release();
+  vbo_.destroy();
+  ibo_.release();
+  ibo_.destroy();
+  cbo_.release();
+  cbo_.destroy();
+  vao_.release();
+  vao_.destroy();
 }
 
 //////////////////////////////////////////////////////////////////////
 // Privates
+QString BasicWidget::vertexShaderString() const
+{
+  QString str =
+	"#version 330\n"
+	"layout(location = 0) in vec3 position;\n"
+    "layout(location = 1) in vec4 color;\n"
+    "out vec4 vertColor;\n"
+	"void main()\n"
+	"{\n"
+	"  gl_Position = vec4(position, 1.0);\n"
+    "  vertColor = color;\n"
+    "}\n";
+  return str;
+}
+
+QString BasicWidget::fragmentShaderString() const
+{
+  QString str =
+	"#version 330\n"
+    "in vec4 vertColor;\n"
+	"out vec4 color;\n"
+	"void main()\n"
+	"{\n"
+	"  color = vertColor;\n"
+	"}\n";
+  return str;
+}
+
+void BasicWidget::createShader()
+{
+  QOpenGLShader vert(QOpenGLShader::Vertex);
+  vert.compileSourceCode(vertexShaderString());
+  QOpenGLShader frag(QOpenGLShader::Fragment);
+  frag.compileSourceCode(fragmentShaderString());
+  bool ok = shaderProgram_.addShader(&vert);
+  if(!ok) {
+	qDebug() << shaderProgram_.log();
+  }
+  ok = shaderProgram_.addShader(&frag);
+  if(!ok) {
+	qDebug() << shaderProgram_.log();
+  }
+  ok = shaderProgram_.link();
+  if(!ok) {
+	qDebug() << shaderProgram_.log();
+  }
+}
 
 ///////////////////////////////////////////////////////////////////////
 // Protected
 void BasicWidget::keyReleaseEvent(QKeyEvent* keyEvent)
 {
-  // TODO
-  // Handle key events here.
-  qDebug() << "You Pressed an unsupported Key!";
-  // ENDTODO
+  // TODO: Handle key events here.
+  if (keyEvent->key() == Qt::Key_Left) {
+    qDebug() << "Left Arrow Pressed";
+    update();  // We call update after we handle a key press to trigger a redraw when we are ready
+  } else if (keyEvent->key() == Qt::Key_Right) {
+    qDebug() << "Right Arrow Pressed";
+    update();  // We call update after we handle a key press to trigger a redraw when we are ready
+  } else {
+    qDebug() << "You Pressed an unsupported Key!";
+  }
 }
 void BasicWidget::initializeGL()
 {
@@ -36,6 +97,12 @@ void BasicWidget::initializeGL()
   qDebug() << "  Renderer: " << reinterpret_cast<const char*>(glGetString(GL_RENDERER));
   qDebug() << "  Version: " << reinterpret_cast<const char*>(glGetString(GL_VERSION));
   qDebug() << "  GLSL Version: " << reinterpret_cast<const char*>(glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+  // Set up our shaders.
+  createShader();
+	
+	// TODO: load obj data
+	
 
   glViewport(0, 0, width(), height());
 }
@@ -54,4 +121,9 @@ void BasicWidget::paintGL()
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   // TODO:  render.
+  shaderProgram_.bind();
+  vao_.bind();
+  glDrawElements(GL_TRIANGLES, ??, GL_UNSIGNED_INT, 0);
+  vao_.release();
+  shaderProgram_.release();
 }
