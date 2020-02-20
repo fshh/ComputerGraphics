068306709f828f48c11e3ed2cf3d147f8505a7b3
fshh <ajknollmeyer@gmail.com>
2020-02-20 16:22:33 -0500

    Wireframe

--- Assignment3_ModelParser/BasicWidget.cpp
+++ Assignment3_ModelParser/BasicWidget.cpp
@@ -42,7 +42,7 @@ void BasicWidget::keyReleaseEvent(QKeyEvent* keyEvent)
 	else if (keyEvent->key() == Qt::Key_W) {
 		qDebug() << "W Pressed";
 		wireframe = !wireframe;
-		//glPolygonMode(GL_FRONT_AND_BACK, wireframe ? GL_LINE : GL_FILL);
+		glPolygonMode(GL_FRONT_AND_BACK, wireframe ? GL_LINE : GL_FILL);
 		update();  // We call update after we handle a key press to trigger a redraw when we are ready
   } else {
     qDebug() << "You Pressed an unsupported Key!";
